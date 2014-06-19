99e615835a0c818699ff0f3dd812d1c91f40cb5d
minhaz <minhazav@gmail.com>
2014-06-20 04:54:18 +0530

    php code made secure
    #Modification:
    1. application does not break if any credentials not sent
    2. db parameters defined at the top, #todo: need to move it to one config file
    3. Code indentations and `psr-2` coding standard implemented

--- online_form.php
+++ online_form.php
@@ -1,12 +1,16 @@
 <?php
-$connect = mysql_connect("localhost","root","");
-if(!$connect) die ("connection not established".mysql_error());
+define("host", "localhost");
+define("user", "root");
+define("password", "");
+define("db", "forms");
 
-mysql_select_db("forms",$connect);
+$connect = mysql_connect(host, user, password);
+if(!$connect) die ("connection not established" .mysql_error());
+
+mysql_select_db(db, $connect);
 
 
-function testinput($data)
-{
+function testinput($data) {
 	$data = stripslashes($data);
 	$data = htmlspecialchars($data);
 	$data = trim($data);
@@ -14,48 +18,49 @@ function testinput($data)
 }
 
 
-if($_POST)
-{
-$firstname   = testinput($_POST['firstname']);
-$lastname    = testinput($_POST['lastname']);
-$currentyear = testinput($_POST['currentyear']);
-$email       = testinput($_POST['email']);
-$contact     = testinput($_POST['tel']);
-$yearsjoined = testinput($_POST['yearsjoined']);
-$fees        = testinput($_POST['fees']);
-}
-
-$_SESSION['firstname'] = $firstname;
-$_SESSION['lasttname'] = $lastname;
-$name = $_SESSION['firstname'] . "" . $_SESSION['firstname'];
-
-if(!is_numeric($contact))
-		{
-			header("location: fill_form.php?error=1");
+if ($_POST) {
+	// Make sure we recieved each input or the application will break
+	$reqInputs = ['firstname', 'lastname', 'currentyear', 'email', 'tel', 'yearsjoined', 'fees'];
+	foreach($reqInputs as $x) {
+		if (!isset($_POST[$x])) {
+			header("location: fill_form.php?error=1&message=1003&item-name=$x");
+			//1003 stands for insufficient credentials
 			exit;
 		}
+	}
 
-//Update database
+	$firstname   = testinput($_POST['firstname']);
+	$lastname    = testinput($_POST['lastname']);
+	$currentyear = testinput($_POST['currentyear']);
+	$email       = testinput($_POST['email']);
+	$contact     = testinput($_POST['tel']);
+	$yearsjoined = testinput($_POST['yearsjoined']);
+	$fees        = testinput($_POST['fees']);
+	}
 
+	$_SESSION['firstname'] = $firstname;
+	$_SESSION['lasttname'] = $lastname;
+	$name = $_SESSION['firstname'] . " " . $_SESSION['lastname'];
+
+	if (!is_numeric($contact)) {
+		header("location: fill_form.php?error=1");
+		exit;
+	}
+
+	//Update database
 	$query = "INSERT INTO information(name,current,email,contact,joinedfor,fees)
 		VALUES('$name','$currentyear','$email','$contact','$yearsjoined','$fees');";
 	$sql = mysql_query($query);
 
-	if(!$sql)
-		{
-			echo mysql_error();
-			exit;
-		}
-	
-
-	else
-	{
+	if (!$sql) {
+		echo mysql_error();
+		exit;
+	} else {
 		echo "Your response has been recorded";
 		header("location : index.html");
+		exit;
 	}
 
 	mysql_close($connect);
 	header("location : index.html");
-	
-
 ?>
