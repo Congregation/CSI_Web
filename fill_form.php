92237909e8348386c7fa155db5f263a48da562b7
Ashutosh Kumar <ashutosh11939@gmail.com>
2014-06-23 15:28:16 +0530

    google analytics + slight changes

--- fill_form.php
+++ fill_form.php
@@ -154,7 +154,7 @@
 			<div class="row animated opacity mar-bot20" data-andown="fadeIn" data-animation="animation">
 				<div class="col-sm-12 align-center">
                     <ul class="social-network social-circle">
-                        <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="https://www.facebook.com/PhoneixCSIDTU" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
                         <li><a href="https://twitter.com/CSIDTU" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
                         <li><a href="https://plus.google.com/u/0/b/106882834815777278175" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
                         <li><a href="http://www.linkedin.com/groups/CSI-DTU-Updates-6714603" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i>
@@ -169,7 +169,20 @@
 		</div>
 
 
+<!-- #################################       Google analytics        ############################### -->
 
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-52178544-1', 'csidtu.com');
+	  ga('send', 'pageview');
+
+	</script>
+
+	<!-- ###################  -->
 
 
 <a href="#header" class="scrollup"><i class="fa fa-chevron-up"></i></a>	
